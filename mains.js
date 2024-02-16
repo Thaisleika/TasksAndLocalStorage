9c40c057b621b919a7f46241e2de1e271fb53c5c
Thais Leika <thaisleika@outlook.com>
2024-02-16 16:05:42 +0000

    modified main.js

--- mains.js
+++ mains.js
@@ -1,46 +1,50 @@
 import { tasksManager } from './tasksManager.js';
 
 document.addEventListener('DOMContentLoaded', () => {
- const addTaskButton = document.getElementById('addTaskButton');
- const taskInput = document.getElementById('taskInput');
- const taskList = document.getElementById('taskList');
+  const addTaskButton = document.getElementById('addTaskButton');
+  const taskInput = document.getElementById('taskInput');
+  const taskList = document.getElementById('taskList');
 
   function TaskPage() {
-     const tasks = tasksManager.getTasks();
-      taskList.innerHTML = "";
-      
-      tasks.forEach((task, index) => {
-       const list = document.createElement('li');
-       list.className = 'task-item';
-       list.innerHTML = `<p>${task}</p> <p class='delete-btn'> X </p>`;
+    const tasks = tasksManager.getTasks();
+    taskList.innerHTML = "";
+    
+    tasks.forEach((task, index) => {
+      const listItem = document.createElement('li');
+      listItem.className = 'task-item';
+      listItem.innerHTML = `
+        <p>${task}</p>
+        <p class='delete-btn'>X</p>
+      `;
 
-       list.addEventListener('click', () => {
-        list.classList.toggle('done');
-       });
+      listItem.addEventListener('click', () => {
+        listItem.classList.toggle('done');
+      });
 
-       list.children[1].addEventListener('click', () =>{
+      listItem.children[1].addEventListener('click', () => {
         tasksManager.removeTask(index);
         TaskPage();
-       });
+      });
 
-       taskList.appendChild(list);
-      })
-   }
-   function validateTaskInput() {
-      const task = taskInput.value;
-      if (task) {
-         tasksManager.addtask(task);
-         taskInput.value= '';
-         TaskPage();
-      }
-   }
+      taskList.appendChild(listItem);
+    });
+  }
 
-   addTaskButton.addEventListener('click', validateTaskInput);
-   taskInput.addEventListener('keydown', (event) => {
-      if (event.key === 'Enter') {
-         validateTaskInput();
-      }
-   });
+  function validateTaskInput() {
+    const task = taskInput.value;
+    if (task) {
+      tasksManager.addTask(task);
+      taskInput.value = '';
+      TaskPage();
+    }
+  }
 
-   TaskPage();
+  addTaskButton.addEventListener('click', validateTaskInput);
+  taskInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      validateTaskInput();
+    }
+  });
+
+  TaskPage();
 });
